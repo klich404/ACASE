74047596ba212be9fa50c82760555caeb21ac9e8
natyarteagac <natalia.arteagac94@gmail.com>
2021-10-25 18:36:02 -0500

    Feature:  POST request for Back-End

--- ACASE_front/ACASE/javascript/cards.js
+++ ACASE_front/ACASE/javascript/cards.js
@@ -31,7 +31,6 @@ class Inbox {
       }
     })();
   };
-
 // This function create the tags and give the respective values
   makeCards(cards) {
     let htmlElements = ``
@@ -40,10 +39,10 @@ class Inbox {
       <div class="card">
       <div class="card-body p-2">
       <h5 class="card-title mb-1">${element.title}</h5>
-      <p class="card-text mb-2"> ${element.text}
+      <p class="card-text mb-2"> ${this.truncateText(element.text)}
       </p>
       <p class="date mb-1"><b>Fecha: </b>${element.date}</p>
-      <p class="source-url mb-2"><b>Fuente:</b> ${element.url}/</p>
+      <p class="source-url mb-2"><b>Fuente:</b> ${element.url}/</p> 
       <a href="${element.url}" target="_blank" class="btn btn-primary">Visitar</a>
       <a data-id="1175171e-0950-43e2-a881-72ca165c890d-${element.id}" href="#" class="modify-button btn btn-primary">Modificar</a>
       <img id="drop-button" class="trash-icon" src="./icons/trash.png" alt="trash">
@@ -54,6 +53,12 @@ class Inbox {
     });
     return htmlElements
   }
+  truncateText (text, limit = 200) {
+    return (text.length <= limit)
+      ? text
+      : text.slice(0, limit) + "..."
+  }
+
 // This function works with the button "Palabra clave" filtering the cards with
   filterKeyword(keyWord, response) {
     let listObjectbyKey = []
@@ -82,6 +87,44 @@ class Inbox {
     })
     return listObjectKeyAndValue;
   }
+
+// Function to create the html content after click in button "Modificar"
+    createOverlay(dataId) {
+      let title = document.getElementById(dataId).querySelector('h5').innerText;
+      let formPage =  `<div class="container-fluid vh-100 prompt-overlay">
+        <div class="prompt">
+          <h2 class="title-text-form">${title}</h2>
+          <form action="http://127.0.0.1:8000/form/" method="POST">
+            <div class="form-control">
+              <label class="form-questions" for="relevance">¿Por qué es relevante este artículo?</label>
+              <textarea id="relevance" cols="60" rows="2" placeholder="Ingresa tu texto" name="relevance"
+                class="input-field"></textarea>
+            </div>
+            <div class="form-control">
+              <label class="form-questions" for="learning">¿Qué vas a aprender de este artículo?</label>
+              <textarea id="learning" cols="60" rows="2" placeholder="Ingresa tu texto" name="learning"
+                class="input-field"></textarea>
+            </div>
+            <div class="form-control">
+              <label class="form-questions" for="finding">¿Cuál es el hallazgo más importante que vas a
+                encontrar aquí?</label>
+              <textarea id="finding" cols="60" rows="2" placeholder="Ingresa tu texto"
+                name="finding" class="input-field"></textarea>
+            </div>
+            <div class="form-control">
+              <label class="form-questions" for="page">Página de inicio y final</label>
+              <textarea id="page" cols="60" rows="2" placeholder="Ingresa tu texto" name="page"
+                class="input-field"></textarea>
+            </div>
+            <button id="close-form">Cerrar</button>
+            <input type="hidden" value="${dataId}" name="id">
+            <input type="submit" value="Enviar" id="submit-btn" class="submit-btn">
+          </form>
+        </div>
+      </div>`
+      console.log(formPage)
+      return formPage
+    }
 // Function to listen click of Button "Modificar" and open the form with the method createOverlay
   showForm() {
     document.querySelectorAll('.modify-button').forEach(e => {
@@ -96,7 +139,6 @@ class Inbox {
       })
     })
   }
-
 // Function to close the form after click
   closeForm(dataId) {
     document.getElementById('close-form').addEventListener('click', () => {
@@ -104,40 +146,7 @@ class Inbox {
     });
   }
 
-// Function to create the html content after click in button "Modificar"
-  createOverlay(dataId) {
-    let title = document.getElementById(dataId).querySelector('h5').innerText;
-    return ` <div class="container-fluid vh-100 prompt-overlay">
-      <div class="prompt">
-        <h2 class="title-text-form">${title}</h2>
-        <form class="submit-form">
-          <div class="form-control">
-            <label class="form-questions" for="relevant-message">¿Por qué es relevante este artículo?</label>
-            <textarea id="relevant-message" cols="60" rows="2" placeholder="Ingresa el texto" name="relevant_message"
-              class="input-field"></textarea>
-          </div>
-          <div class="form-control">
-            <label class="form-questions" for="relevant-learning">¿Qué vas a aprender de este artículo?</label>
-            <textarea id="relevant-learning" cols="60" rows="2" placeholder="Ingresa el texto" name="relevant_learning"
-              class="input-field"></textarea>
-          </div>
-          <div class="form-control">
-            <label class="form-questions" for="relevant-encounter">¿Cuál es el hallazgo más importante que vas a
-              encontrar aquí?</label>
-            <textarea id="relevant-encounter" cols="60" rows="2" placeholder="Ingresa el texto"
-              name="relevant_encounter" class="input-field"></textarea>
-          </div>
-          <div class="form-control">
-            <label class="form-questions" for="pages">Página de inicio y final</label>
-            <textarea id="pages" cols="60" rows="2" placeholder="Ingresa el texto" name="pages"
-              class="input-field"></textarea>
-          </div>
-          <button id="close-form">Cerrar</button>
-          <input type="submit" value="Enviar" id="submit-btn" class="submit-btn">
-        </form>
-      </div>
-    </div>`
-  }
+
 }
 
 export { Inbox }
