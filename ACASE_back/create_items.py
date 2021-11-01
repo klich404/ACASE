b2f78f45a72f40953f26e5c2afe622a77f7cdd98
klich404 <klich4010@gmail.com>
2021-11-01 16:08:08 -0500

    code comments

--- ACASE_back/create_items.py
+++ ACASE_back/create_items.py
@@ -23,32 +23,24 @@ with open("data.json", "r") as data:
                 kws = list(set(kws)) #save all the keywords
             if key == 'Url':
                 if Item.objects.filter(Url=value).count() >= 1: #don't repeat items
-                    #print("This Item already exists")
                     pass
                 else:
                     Item.objects.create(**element) #create the new items
-                    #print("New Item created!")
                     for kw in kws:
                         if Keyword.objects.filter(Word=kw).count() >= 1: #don't repeat keywords
-                            #print("This Keyword already exists")
                             pass
                         else:
                             Keyword.objects.create(Word=kw) #create the new keyword
-                            #print("New Keyword created!")
                     for word in kws:
                         objs = Item.objects.filter(Associated_KW=word)
                         kw = Keyword.objects.get(Word=word)
                         kw.Items.add(*objs) #create new m2m relationship
-                        #print("New relation Keyword-Item created!")
                     for sour_url in source_url:
                         if Target.objects.filter(Base_url=sour_url).count() >= 1: #don't repeat urls
-                            #print("This Target already exists")
                             pass
                         else:
                             Target.objects.create(Base_url=sour_url) #create the new url
-                            #print("New Target created!")
                     for s_url in source_url:
                         objs = Item.objects.filter(Associated_KW=s_url)
                         url = Target.objects.get(Base_url=s_url)
                         url.Items.add(*objs) #create new m2m relationship
-                        #print("New relation Target-Item created!!")
