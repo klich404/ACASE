93abbc93b6c408c795108da0d197a613ec58a068
natyarteagac <natalia.arteagac94@gmail.com>
2021-11-01 21:19:03 -0500

    Updating work with functional buttons and cleaning code

--- ACASE_front/ACASE/javascript/trash-cards.js
+++ ACASE_front/ACASE/javascript/trash-cards.js
@@ -9,34 +9,31 @@ class trashCards {
           if (e.My_selection == false && e.Trash_section == true)
           responseList.push(e)
         })
-        console.log(responseList);
         let cards = this.makeCards(responseList);
         document.getElementById('cards-container').innerHTML = cards;
-        /*this.showForm();
-        this.toMySelection();
-        this.toTrash();*/
+        this.getAndShowModifiedData(responseList);
       } catch (error) {
         console.log(error);
       }
     })();
   }
+// Function to make the html card
   makeCards(cards) {
     let htmlElements = ``
     cards.forEach(element => {
-      htmlElements += `<div id="${element.Id}" class="col">
-      <div class="card m-2">
+      htmlElements += `<div id="${element.id}" class="col">
+      <div class="card mb-2">
       <div class="card-body">
       <div>
-      <h5 class="card-title mb-1">${element.Title}</h5>
-      <p class="card-text mb-2"> ${this.truncateText(element.Text)}
-      </p>
-      <p class="date mb-1"><b>Fecha: </b>${element.Date}</p>
-      <p class="source-url mb-2"><b>Fuente:</b> ${element.Url}/</p>
+      <h3 class="card-title mb-3">${this.truncateTitle(element.Title)}</h3>
+      <p class="card-text mb-2"> ${this.truncateText(element.Text)}</p>
       </div>
       <div>
+      <p class="date mb-1"><b>Fecha: </b>${element.Date}</p>
+      <p class="source-url"><b>Fuente:</b> ${this.truncateUrl(element.Url)}/</p>
       <a href="${element.Url}" target="_blank" class="btn btn-primary">Visitar</a>
-      <a data-id="${element.Id}" href="#" class="modify-button btn btn-primary">Modificar</a>
-      <img id="${element.Id}" class="check-icon" src="./icons/check-circle-regular.svg" alt="check">
+      <a data-id="${element.id}" href="#" class="modify-button btn btn-primary">Visualizar</a>
+      <img id="${element.id}" class="trash-icon" src="./icons/times-circle-regular.svg" alt="trash">
       </div>
       </div>
       </div>
@@ -44,13 +41,83 @@ class trashCards {
     });
     return htmlElements
   }
-  // Truncate the text with 180 words 
+
+// Truncate Title with 90 words
+  truncateTitle(text, limit = 90) {
+    return (text.length <= limit)
+      ? text
+      : text.slice(0, limit) + "..."
+  }
+// Truncate the text with 180 words 
   truncateText(text, limit = 180) {
     return (text.length <= limit)
       ? text
       : text.slice(0, limit) + "..."
   }
+// Truncate the text with 90 words
+  truncateUrl(text, limit = 90) {
+    return (text.length <= limit)
+      ? text
+      : text.slice(0, limit) + "..."
+  }
 
+// Return the object selected by user
+  getAndShowModifiedData(data) {
+    document.querySelectorAll('.modify-button').forEach((e, i) => {
+      e.addEventListener('click', () => {
+        let form = this.showPromptOverlay(data[i]);
+        this.renderModifiedData(form);
+        this.closeForm();
+      })
+    })
+  }
+// Render the data sent by the function getAndShowModifiedData
+  renderModifiedData(form) {
+    let body = document.querySelector('body');
+    let parentElement = document.createElement('div');
+    parentElement.setAttribute('id', 'div-form');
+    parentElement.innerHTML = form;
+    body.appendChild(parentElement);
+  }
+  // Create the html to view the data selection
+  showPromptOverlay(data) {
+    return `<div class="container-fluid prompt-overlay">
+      <div class="container-fluid prompt">
+      <h2 class="title-text-form">${data.Title}</h2>
+      <div>
+        <h4 class="subtitle-visualization">¿Por qué es relevante este artículo?</h4>
+        <textarea name="" id="input-field" cols="60"
+          rows="3">${data.Relevance}</textarea>
+      </div>
+      <div>
+        <h4 class="subtitle-visualization">¿Qué aprendiste de este artículo?</h4>
+        <textarea name="" id="input-field" cols="60"
+          rows="3">${data.Finding}</textarea>
+      </div>
+      <div>
+        <h4 class="subtitle-visualization">¿Cuál es el hallazgo más importa que encontraste?</h4>
+        <textarea name="" id="input-field" cols="60"
+          rows="3">${data.Learning}</textarea>
+      </div>
+      <div>
+        <h4 class="subtitle-visualization">Página de inicio y final</h4>
+        <textarea name="" id="input-field" cols="60"
+          rows="3">${data.Pages}</textarea>
+      </div>
+      <button class="close-button-visualization">Cerrar</button>
+    </div>
+  </div>
+     `
+  }
+  // Function to close the form after click Cerrar
+  closeForm() {
+    document.querySelectorAll('.close-button-visualization').forEach(e => {
+      e.addEventListener('click', () => {
+        console.log('click')
+        document.getElementById('div-form').remove();
+      })
+    });
+  }
 }
 
 export { trashCards }
